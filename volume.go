be5c1b7e21dea9b518742ba8fe47d66e0a7c5bf5
Peter Jönsson <peter.jonsson@klarna.com>
2014-01-13 01:54:37 +0100

    Move out Volume inner struct

--- volume.go
+++ volume.go
@@ -24,34 +24,36 @@ func (c CloudStackClient) ListVolumes(vmid string) (string, error) {
 	return volumeId, err
 }
 
+type Volume struct {
+	Account                    string        `json:"account"`
+	Created                    string        `json:"created"`
+	Destroyed                  bool          `json:"destroyed"`
+	Deviceid                   float64       `json:"deviceid"`
+	Domain                     string        `json:"domain"`
+	Domainid                   string        `json:"domainid"`
+	ID                         string        `json:"id"`
+	Isextractable              bool          `json:"isextractable"`
+	Name                       string        `json:"name"`
+	Serviceofferingdisplaytext string        `json:"serviceofferingdisplaytext"`
+	Serviceofferingid          string        `json:"serviceofferingid"`
+	Serviceofferingname        string        `json:"serviceofferingname"`
+	Size                       float64       `json:"size"`
+	State                      string        `json:"state"`
+	Storage                    string        `json:"storage"`
+	Storagetype                string        `json:"storagetype"`
+	Tags                       []interface{} `json:"tags"`
+	Type                       string        `json:"type"`
+	Virtualmachineid           string        `json:"virtualmachineid"`
+	Vmdisplayname              string        `json:"vmdisplayname"`
+	Vmname                     string        `json:"vmname"`
+	Vmstate                    string        `json:"vmstate"`
+	Zoneid                     string        `json:"zoneid"`
+	Zonename                   string        `json:"zonename"`
+}
+
 type ListVolumesResponse struct {
 	Listvolumesresponse struct {
-		Count  float64 `json:"count"`
-		Volume []struct {
-			Account                    string        `json:"account"`
-			Created                    string        `json:"created"`
-			Destroyed                  bool          `json:"destroyed"`
-			Deviceid                   float64       `json:"deviceid"`
-			Domain                     string        `json:"domain"`
-			Domainid                   string        `json:"domainid"`
-			ID                         string        `json:"id"`
-			Isextractable              bool          `json:"isextractable"`
-			Name                       string        `json:"name"`
-			Serviceofferingdisplaytext string        `json:"serviceofferingdisplaytext"`
-			Serviceofferingid          string        `json:"serviceofferingid"`
-			Serviceofferingname        string        `json:"serviceofferingname"`
-			Size                       float64       `json:"size"`
-			State                      string        `json:"state"`
-			Storage                    string        `json:"storage"`
-			Storagetype                string        `json:"storagetype"`
-			Tags                       []interface{} `json:"tags"`
-			Type                       string        `json:"type"`
-			Virtualmachineid           string        `json:"virtualmachineid"`
-			Vmdisplayname              string        `json:"vmdisplayname"`
-			Vmname                     string        `json:"vmname"`
-			Vmstate                    string        `json:"vmstate"`
-			Zoneid                     string        `json:"zoneid"`
-			Zonename                   string        `json:"zonename"`
-		} `json:"volume"`
+		Count  float64  `json:"count"`
+		Volume []Volume `json:"volume"`
 	} `json:"listvolumesresponse"`
 }
